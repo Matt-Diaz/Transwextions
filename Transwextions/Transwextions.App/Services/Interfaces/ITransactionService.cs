37b9c173a4e0b33248b80e68c5fa1f34869eb0df
Matthew Diaz <matthew.diaz@hp.com>
2025-10-17 19:43:51 -0400

    - Created transaction service - Add a scoped transaction service - Update transaction model to contain an IsDeleted flag

---
+++ Transwextions/Transwextions.App/Services/Interfaces/ITransactionService.cs
@@ -0,0 +1,11 @@
+﻿using Transwextions.Data.Models;
+
+namespace Transwextions.App.Services.Interfaces;
+
+public interface ITransactionService
+{
+    public Task<ServiceResult<List<TransactionModel>>> GetAllAsync();
+    public Task<ServiceResult<object>> AddAsync(TransactionModel model, DateTime? transactionDateOverride = null);
+    public Task<ServiceResult<object>> DeleteAsync(Guid transactionGuid);
+    public Task<ServiceResult<TransactionModel?>> GetByGuidAsync(Guid transactionGuid);
+}
