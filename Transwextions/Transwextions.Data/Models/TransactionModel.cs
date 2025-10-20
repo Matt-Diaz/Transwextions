0cc6914e1229033b25a5d6dc800de4db9e537ec4
Matthew Diaz <matthew.diaz@hp.com>
2025-10-20 19:01:10 -0400

    - Removed Title column from transactions model - Added length constraint for transaction description

--- Transwextions/Transwextions.Data/Models/TransactionModel.cs
+++ Transwextions/Transwextions.Data/Models/TransactionModel.cs
@@ -1,10 +1,15 @@
-﻿namespace Transwextions.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Transwextions.Data.Models;
 
 public class TransactionModel
 {
     public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
+
+    [StringLength(50, MinimumLength = 1)]
+    [Required]
     public string Description { get; set; } = string.Empty;
+
     public ulong AmountTotalCents { get; set; }
     public DateTime TransactionDateUtc { get; set; }
     public Guid? UniqueIdentifier { get; set; }
